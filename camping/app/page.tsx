f4fb1d88faae745f913d5d6e7c18b8b0055fa44e
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2025-05-30 21:23:25 +0700

    install shade_ui

---
+++ camping/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+const page = () => {
+  return (
+    <>
+      <div>page</div>
+      <h1>Hello</h1>
+      <Button variant="secondary" size="lg">Button</Button>
+    </>
+  );
+};
+export default page;
