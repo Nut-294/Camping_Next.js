40b65a1dcc9a86a3a304e7c0a67f9dcbc30f7a21
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2025-05-30 21:50:51 +0700

    Add Navbar Logo Search Input

---
+++ camping/components/Navbar/Search.tsx
@@ -0,0 +1,10 @@
+import { Input } from "@/components/ui/input"
+const Search = () => {
+  return (
+    <Input 
+    type="text"
+    placeholder="Search Campping..."
+    className="max-w-xs"/>
+  )
+}
+export default Search
