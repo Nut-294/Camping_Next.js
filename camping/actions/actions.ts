070105f10ccf7bb0449478b0b2e80feb65e96905
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2025-06-07 13:23:29 +0700

    Create Profile &Clerk Id

--- camping/actions/actions.ts
+++ camping/actions/actions.ts
@@ -1,5 +1,20 @@
 "use server";
 import { profileSchema, validateWithZod } from "@/utils/schemas";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import db from "@/utils/db";
+import { redirect } from "next/navigation";
+
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  console.log('user จาก clerk',user)
+  if (!user) {
+    throw new Error("you must logged!!");
+  }
+ //ถ้าไม่มี Profile ใ้ห้ไปหน้า ลงทะเบียน
+  if(!user.privateMetadata.hasProfile) redirect('profile/create')
+  return user;
+};
 
 const renderError = (error: unknown): { message: string } => {
   return {
@@ -12,12 +27,35 @@ export const createProfileAction = async (
   formData: FormData
 ) => {
   try {
+    const user = await getAuthUser();
+
     const rawData = Object.fromEntries(formData);
     const validateField = validateWithZod(profileSchema, rawData);
-    console.log("validated", validateField);
-    return { message: "Create Profile Success!!" };
+    console.log("validated จากหน้าบ้าน", validateField);
+    //ค่าตัวแปรจากหน้าบ้านต้องตรงกับ schema ในฐานข้อมูล พิมเล็กพิมใหญ่ต้องตรง เพราะเราเอาค่าไปคัดลองลงฐานข้อมูลเลย
+
+    await db.profile.create({
+      //log ดูค่า userเพื่อดูว่าต้องเข้าถึงตัวไหน
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        // ... คือ คัดลอก Object
+        ...validateField,
+      },
+    });
+
+    const client = await clerkClient();
+    await client.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+
+    // return { message: "Create Profile Success!!" };
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return renderError(error);
   }
+  redirect("/");
 };
