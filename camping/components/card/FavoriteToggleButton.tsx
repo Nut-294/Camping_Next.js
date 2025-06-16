d57e4bf33d3c6f30f2ddf1e581526054d61e225a
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2025-06-16 09:01:26 +0700

    Heart SignInCard Clerk

--- camping/components/card/FavoriteToggleButton.tsx
+++ camping/components/card/FavoriteToggleButton.tsx
@@ -1,9 +1,18 @@
 import { Heart } from "lucide-react";
 import { Button } from "../ui/button";
-const FavoriteToggleButton = ({ LandmarkId }: { LandmarkId: string }) => {
+import { auth } from "@clerk/nextjs/server";
+import { SignInCardButton } from "../form/Buttons";
+const FavoriteToggleButton = async ({ LandmarkId }: { LandmarkId: string }) => {
+  const { userId } = await auth();
+  console.log("userId", userId);
+
+  if (!userId) {
+    return <SignInCardButton />;
+  }
+
   return (
-    <Button size='icon' variant='outline'>
-      <Heart fill='black'/>
+    <Button size="icon" variant="outline">
+      <Heart />
     </Button>
   );
 };
--- camping/components/form/Buttons.tsx
+++ camping/components/form/Buttons.tsx
@@ -2,7 +2,8 @@
 import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 import { LoaderCircle } from "lucide-react";
-
+import { SignInButton } from "@clerk/nextjs";
+import { Heart } from "lucide-react";
 type btnSize = "default" | "lg" | "sm";
 
 type SubmitButtonProps = {
@@ -20,14 +21,25 @@ export const SubmitButton = ({ className, size, text }: SubmitButtonProps) => {
       size={size}
       className={`${className} capitalize`}
     >
-      {pending ? 
+      {pending ? (
         <>
           <LoaderCircle className="animate-spin" />
           <span>Please Wait</span>
         </>
-       : 
+      ) : (
         <p>{text}</p>
-      }
+      )}
     </Button>
+  );
+};
+
+export const SignInCardButton = () => {
+  return (
+    //modal ไม่ทำให้เกิดการรีหน้า
+    <SignInButton mode='modal'>
+      <Button size="icon" variant='outline'>
+        <Heart />
+      </Button>
+    </SignInButton>
   );
 };
