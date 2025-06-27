d1201aff1a99de570cd369ad5262fac60bf04009
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2025-06-27 20:52:48 +0700

    ImageContainer

--- camping/app/landmark/[id]/page.tsx
+++ camping/app/landmark/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { fetchLandmarkDetail } from "@/actions/actions";
 import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import Breadcrumbs from "@/components/landmark/Breadcrumbs";
+import ImageContainer from "@/components/landmark/ImageContainer";
 import { redirect } from "next/navigation";
 
 const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
@@ -18,6 +19,7 @@ const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
           <FavoriteToggleButton landmarkId={landmark.id} />
         </div>
       </header>
+      <ImageContainer mainImage={landmark.image} name={landmark.name}/>
     </section>
   );
 };
