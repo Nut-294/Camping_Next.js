d042a3142c0c0e416937624265bd2f9217c74740
Nut-294 <134218655+Nut-294@users.noreply.github.com>
2025-06-30 13:22:28 +0700

    Share

--- camping/app/landmark/[id]/page.tsx
+++ camping/app/landmark/[id]/page.tsx
@@ -3,6 +3,7 @@ import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import Breadcrumbs from "@/components/landmark/Breadcrumbs";
 import Description from "@/components/landmark/Description";
 import ImageContainer from "@/components/landmark/ImageContainer";
+import ShareButton from "@/components/landmark/ShareButton";
 import MapLandmark from "@/components/map/Map";
 
 import { redirect } from "next/navigation";
@@ -18,7 +19,7 @@ const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
       <header className="flex justify-between items-center mt-4">
         <h1 className="text-4xl font-bold">{landmark.name}</h1>
         <div className="flex items-center gap-x-4">
-          <span>share</span>
+          <ShareButton landmarkId={landmark.id} name={landmark.name}/>
           <FavoriteToggleButton landmarkId={landmark.id} />
         </div>
       </header>
